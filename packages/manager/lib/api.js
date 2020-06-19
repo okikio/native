0d934c6c829ddf2d61e330fd24fd919b84b3499e
Okiki Ojo <femikiki@gmail.com>
2020-06-19 18:41:27 -0400

    No longer directly extending Map

--- packages/manager/lib/api.js
+++ packages/manager/lib/api.js
@@ -1,31 +1,47 @@
 /*!
- * managerjs v1.0.7
+ * managerjs v1.0.8
  * (c) 2020 Okiki Ojo
  * Released under the MIT license
  */
 
 'use strict';
 
-Object.defineProperty(exports, '__esModule', { value: true });
-
 /**
- * Manages complex lists of named data, eg. A page can be stored in a list by of other pages with the url being how the page is stored in the list.
+ * Manages complex lists of named data, eg. A page can be stored in a list by of other pages with the url being how the page is stored in the list. Managers use Maps to store data.
  *
  * @export
  * @class Manager
- * @extends {Map<K, V>}
  * @template K
  * @template V
-*/
-class Manager extends Map {
+ */
+class Manager {
     /**
      * Creates an instance of Manager.
      *
-     * @param {...any} args
+     * @param {Array<[K, V]>} value
      * @memberof Manager
      */
-    constructor(...args) {
-        super(...args);
+    constructor(value) {
+        this.map = new Map(value);
+    }
+    /**
+     * Returns the Manager class's list
+     *
+     * @returns Map<K, V>
+     * @memberof Manager
+     */
+    getMap() {
+        return this.map;
+    }
+    /**
+     * Get a value stored in the Manager
+     *
+     * @public
+     * @param  {K} key - The key to find in the Manager's list
+     * @returns V
+     */
+    get(key) {
+        return this.map.get(key);
     }
     /**
      * Returns the keys of all items stored in the Manager as an Array
@@ -34,7 +50,7 @@ class Manager extends Map {
      * @memberof Manager
      */
     keys() {
-        return Array.from(super.keys.call(this));
+        return [...this.map.keys()];
     }
     /**
      * Returns the values of all items stored in the Manager as an Array
@@ -43,7 +59,40 @@ class Manager extends Map {
      * @memberof Manager
      */
     values() {
-        return Array.from(super.values.call(this));
+        return [...this.map.values()];
+    }
+    /**
+     * Set a value stored in the Manager
+     *
+     * @public
+     * @param  {K} key - The key where the value will be stored
+     * @param  {V} value - The value to store
+     * @returns Manager<K, V>
+     */
+    set(key, value) {
+        this.map.set(key, value);
+        return this;
+    }
+    /**
+     * Adds a value to Manager, and uses the current size of the Manager as it's key, it works best when all the key in the Manager are numbers
+     *
+     * @public
+     * @param  {V} value
+     * @returns Manager<K, V>
+     */
+    add(value) {
+        // @ts-ignore
+        this.map.set(this.size, value);
+        return this;
+    }
+    /**
+     * Returns the total number of items stored in the Manager
+     *
+     * @public
+     * @returns Number
+     */
+    get size() {
+        return this.map.size;
     }
     /**
      * Returns the last item in the Manager who's index is a certain distance from the last item in the Manager
@@ -66,18 +115,68 @@ class Manager extends Map {
         return this.last(2);
     }
     /**
-     * Adds a value to Manager, and uses the current size of the Manager as it's key, it works best when all the key in the Manager are numbers
+     * Removes a value stored in the Manager, via the key
      *
      * @public
-     * @param  {V} value
+     * @param  {K} key - The key for the key value pair to be removed
      * @returns Manager<K, V>
      */
-    add(value) {
-        super.set.call(this, this.size, value);
+    delete(key) {
+        this.map.delete(key);
         return this;
     }
     /**
-     * Calls a method for all items that are currently in it's list
+     * Clear the Manager of all its contents
+     *
+     * @public
+     * @returns Manager<K, V>
+     */
+    clear() {
+        this.map.clear();
+        return this;
+    }
+    /**
+     * Checks if the Manager contains a certain key
+     *
+     * @public
+     * @param {K} key
+     * @returns boolean
+     */
+    has(key) {
+        return this.map.has(key);
+    }
+    /**
+     * Returns a new Iterator object that contains an array of [key, value] for each element in the Map object in insertion order.
+     *
+     * @public
+     * @returns IterableIterator<[K, V]>
+     */
+    entries() {
+        return this.map.entries();
+    }
+    /**
+     * Iterates through the Managers contents, calling a callback function every iteration
+     *
+     * @param {*} [callback=(...args: any): void => { }]
+     * @param {object} context
+     * @returns Manager<K, V>
+     * @memberof Manager
+     */
+    forEach(callback = (...args) => { }, context) {
+        this.map.forEach(callback, context);
+        return this;
+    }
+    /**
+     * Allows iteration via the for..of, learn more: [https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Iterators_and_Generators]
+     *
+     * @returns
+     * @memberof Manager
+     */
+    [Symbol.iterator]() {
+        return this.entries();
+    }
+    /**
+     * Calls the method of a certain name for all items that are currently installed
      *
      * @param {string} method
      * @param {Array<any>} [args=[]]
@@ -91,7 +190,7 @@ class Manager extends Map {
         return this;
     }
     /**
-     * Asynchronously calls a method for all items that are currently in it's list, similar to methodCall except the loop waits for the asynchronous method to run, before the loop continues on to the the next method
+     * Asynchronously calls the method of a certain name for all items that are currently installed, similar to methodCall
      *
      * @param {string} method
      * @param {Array<any>} [args=[]]
@@ -99,13 +198,11 @@ class Manager extends Map {
      * @memberof Manager
      */
     async asyncMethodCall(method, ...args) {
-        for await (let [, item] of this) {
-            // @ts-ignore
+        for await (let [, item] of this.map) {
             await item[method](...args);
         }
         return this;
     }
 }
 
-exports.Manager = Manager;
-exports.default = Manager;
+module.exports = Manager;
--- packages/manager/src/api.ts
+++ packages/manager/src/api.ts
@@ -1,21 +1,51 @@
+
 /**
- * Manages complex lists of named data, eg. A page can be stored in a list by of other pages with the url being how the page is stored in the list.
+ * Manages complex lists of named data, eg. A page can be stored in a list by of other pages with the url being how the page is stored in the list. Managers use Maps to store data.
  *
  * @export
  * @class Manager
- * @extends {Map<K, V>}
  * @template K
  * @template V
-*/
-export class Manager<K, V> extends Map<K, V> {
+ */
+export default class Manager<K, V> {
+	/**
+	 * The complex list of named data, to which the Manager controls
+	 *
+	 * @protected
+	 * @type Map<K, V>
+	 * @memberof Manager
+	 */
+    protected map: Map<K, V>;
+
     /**
      * Creates an instance of Manager.
-     *
-     * @param {...any} args
+     * 
+     * @param {Array<[K, V]>} value
      * @memberof Manager
      */
-    constructor(...args: any) {
-        super(...args);
+    constructor(value: Array<[K, V]>) {
+        this.map = new Map(value);
+    }
+
+	/**
+	 * Returns the Manager class's list
+	 *
+	 * @returns Map<K, V>
+	 * @memberof Manager
+	 */
+    public getMap(): Map<K, V> {
+        return this.map;
+    }
+
+	/**
+	 * Get a value stored in the Manager
+	 *
+	 * @public
+	 * @param  {K} key - The key to find in the Manager's list
+	 * @returns V
+	 */
+    public get(key: K): V {
+        return this.map.get(key);
     }
 
 	/**
@@ -24,8 +54,8 @@ export class Manager<K, V> extends Map<K, V> {
 	 * @returns Array<K>
      * @memberof Manager
      */
-    public keys(): any {
-        return Array.from(super.keys.call(this));
+    public keys(): Array<K> {
+        return [...this.map.keys()];
     }
 
 	/**
@@ -34,8 +64,44 @@ export class Manager<K, V> extends Map<K, V> {
 	 * @returns Array<V>
      * @memberof Manager
 	 */
-    public values(): any {
-        return Array.from(super.values.call(this));
+    public values(): Array<V> {
+        return [...this.map.values()];
+    }
+
+	/**
+	 * Set a value stored in the Manager
+	 *
+	 * @public
+	 * @param  {K} key - The key where the value will be stored
+	 * @param  {V} value - The value to store
+	 * @returns Manager<K, V>
+	 */
+    public set(key: K, value: V): Manager<K, V> {
+        this.map.set(key, value);
+        return this;
+    }
+
+    /**
+     * Adds a value to Manager, and uses the current size of the Manager as it's key, it works best when all the key in the Manager are numbers
+     *
+     * @public
+     * @param  {V} value
+     * @returns Manager<K, V>
+     */
+    public add(value: V): Manager<K, V> {
+        // @ts-ignore
+        this.map.set(this.size as K, value);
+        return this;
+    }
+
+	/**
+	 * Returns the total number of items stored in the Manager
+	 *
+	 * @public
+	 * @returns Number
+	 */
+    public get size(): number {
+        return this.map.size;
     }
 
     /**
@@ -60,20 +126,78 @@ export class Manager<K, V> extends Map<K, V> {
         return this.last(2);
     }
 
-    /**
-     * Adds a value to Manager, and uses the current size of the Manager as it's key, it works best when all the key in the Manager are numbers
-     *
-     * @public
-     * @param  {V} value
-     * @returns Manager<K, V>
-     */
-    public add(value: V): Manager<K, V> {
-        super.set.call(this, this.size, value);
+	/**
+	 * Removes a value stored in the Manager, via the key
+	 *
+	 * @public
+	 * @param  {K} key - The key for the key value pair to be removed
+	 * @returns Manager<K, V>
+	 */
+    public delete(key: K): Manager<K, V> {
+        this.map.delete(key);
         return this;
     }
 
 	/**
-	 * Calls a method for all items that are currently in it's list
+	 * Clear the Manager of all its contents
+	 *
+	 * @public
+	 * @returns Manager<K, V>
+	 */
+    public clear(): Manager<K, V> {
+        this.map.clear();
+        return this;
+    }
+
+	/**
+	 * Checks if the Manager contains a certain key
+	 *
+	 * @public
+	 * @param {K} key
+	 * @returns boolean
+	 */
+    public has(key: K): boolean {
+        return this.map.has(key);
+    }
+
+	/**
+	 * Returns a new Iterator object that contains an array of [key, value] for each element in the Map object in insertion order.
+	 *
+	 * @public
+	 * @returns IterableIterator<[K, V]>
+	 */
+    public entries(): IterableIterator<[K, V]> {
+        return this.map.entries();
+    }
+
+	/**
+	 * Iterates through the Managers contents, calling a callback function every iteration
+	 *
+	 * @param {*} [callback=(...args: any): void => { }]
+	 * @param {object} context
+	 * @returns Manager<K, V>
+	 * @memberof Manager
+	 */
+    public forEach(
+        callback: any = (...args: any): void => { },
+        context?: object
+    ): Manager<K, V> {
+        this.map.forEach(callback, context);
+        return this;
+    }
+
+    /**
+     * Allows iteration via the for..of, learn more: [https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Iterators_and_Generators]
+     *
+     * @returns
+     * @memberof Manager
+     */
+    public [Symbol.iterator]() {
+        return this.entries();
+    }
+
+	/**
+	 * Calls the method of a certain name for all items that are currently installed
 	 *
 	 * @param {string} method
 	 * @param {Array<any>} [args=[]]
@@ -88,7 +212,7 @@ export class Manager<K, V> extends Map<K, V> {
     }
 
 	/**
-	 * Asynchronously calls a method for all items that are currently in it's list, similar to methodCall except the loop waits for the asynchronous method to run, before the loop continues on to the the next method
+	 * Asynchronously calls the method of a certain name for all items that are currently installed, similar to methodCall
 	 *
 	 * @param {string} method
 	 * @param {Array<any>} [args=[]]
@@ -96,12 +220,9 @@ export class Manager<K, V> extends Map<K, V> {
 	 * @memberof Manager
 	 */
     public async asyncMethodCall(method: string, ...args: any): Promise<Manager<K, V>> {
-        for await (let [, item] of this) {
-            // @ts-ignore
+        for await (let [, item] of this.map) {
             await item[method](...args);
         }
         return this;
     }
 }
-
-export default Manager;
