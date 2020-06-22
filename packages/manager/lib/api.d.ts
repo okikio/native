f7f2ac1559093c3ec3b649bb0956552164aed703
Okiki Ojo <femikiki@gmail.com>
2020-06-22 15:00:49 -0400

    build: switched to microbundle

--- packages/manager/lib/api.d.ts
+++ packages/manager/lib/api.d.ts
@@ -1,34 +1,80 @@
 /**
- * Manages complex lists of named data, eg. A page can be stored in a list by of other pages with the url being how the page is stored in the list.
+ * Manages complex lists of named data, eg. A page can be stored in a list by of other pages with the url being how the page is stored in the list. Managers use Maps to store data.
  *
  * @export
  * @class Manager
- * @extends {Map<K, V>}
  * @template K
  * @template V
-*/
-export declare class Manager<K, V> extends Map<K, V> {
+ */
+export default class Manager<K, V> {
+    /**
+     * The complex list of named data, to which the Manager controls
+     *
+     * @protected
+     * @type Map<K, V>
+     * @memberof Manager
+     */
+    protected map: Map<K, V>;
     /**
      * Creates an instance of Manager.
      *
-     * @param {...any} args
+     * @param {Array<[K, V]>} value
      * @memberof Manager
      */
-    constructor(...args: any);
+    constructor(value: Array<[K, V]>);
+    /**
+     * Returns the Manager class's list
+     *
+     * @returns Map<K, V>
+     * @memberof Manager
+     */
+    getMap(): Map<K, V>;
+    /**
+     * Get a value stored in the Manager
+     *
+     * @public
+     * @param  {K} key - The key to find in the Manager's list
+     * @returns V
+     */
+    get(key: K): V;
     /**
      * Returns the keys of all items stored in the Manager as an Array
      *
      * @returns Array<K>
      * @memberof Manager
      */
-    keys(): any;
+    keys(): Array<K>;
     /**
      * Returns the values of all items stored in the Manager as an Array
      *
      * @returns Array<V>
      * @memberof Manager
      */
-    values(): any;
+    values(): Array<V>;
+    /**
+     * Set a value stored in the Manager
+     *
+     * @public
+     * @param  {K} key - The key where the value will be stored
+     * @param  {V} value - The value to store
+     * @returns Manager<K, V>
+     */
+    set(key: K, value: V): Manager<K, V>;
+    /**
+     * Adds a value to Manager, and uses the current size of the Manager as it's key, it works best when all the key in the Manager are numbers
+     *
+     * @public
+     * @param  {V} value
+     * @returns Manager<K, V>
+     */
+    add(value: V): Manager<K, V>;
+    /**
+     * Returns the total number of items stored in the Manager
+     *
+     * @public
+     * @returns Number
+     */
+    get size(): number;
     /**
      * Returns the last item in the Manager who's index is a certain distance from the last item in the Manager
      *
@@ -45,15 +91,53 @@ export declare class Manager<K, V> extends Map<K, V> {
      */
     prev(): V | undefined;
     /**
-     * Adds a value to Manager, and uses the current size of the Manager as it's key, it works best when all the key in the Manager are numbers
+     * Removes a value stored in the Manager, via the key
      *
      * @public
-     * @param  {V} value
+     * @param  {K} key - The key for the key value pair to be removed
      * @returns Manager<K, V>
      */
-    add(value: V): Manager<K, V>;
+    delete(key: K): Manager<K, V>;
     /**
-     * Calls a method for all items that are currently in it's list
+     * Clear the Manager of all its contents
+     *
+     * @public
+     * @returns Manager<K, V>
+     */
+    clear(): Manager<K, V>;
+    /**
+     * Checks if the Manager contains a certain key
+     *
+     * @public
+     * @param {K} key
+     * @returns boolean
+     */
+    has(key: K): boolean;
+    /**
+     * Returns a new Iterator object that contains an array of [key, value] for each element in the Map object in insertion order.
+     *
+     * @public
+     * @returns IterableIterator<[K, V]>
+     */
+    entries(): IterableIterator<[K, V]>;
+    /**
+     * Iterates through the Managers contents, calling a callback function every iteration
+     *
+     * @param {*} [callback=(...args: any): void => { }]
+     * @param {object} context
+     * @returns Manager<K, V>
+     * @memberof Manager
+     */
+    forEach(callback?: any, context?: object): Manager<K, V>;
+    /**
+     * Allows iteration via the for..of, learn more: [https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Iterators_and_Generators]
+     *
+     * @returns
+     * @memberof Manager
+     */
+    [Symbol.iterator](): IterableIterator<[K, V]>;
+    /**
+     * Calls the method of a certain name for all items that are currently installed
      *
      * @param {string} method
      * @param {Array<any>} [args=[]]
@@ -62,7 +146,7 @@ export declare class Manager<K, V> extends Map<K, V> {
      */
     methodCall(method: string, ...args: any): Manager<K, V>;
     /**
-     * Asynchronously calls a method for all items that are currently in it's list, similar to methodCall except the loop waits for the asynchronous method to run, before the loop continues on to the the next method
+     * Asynchronously calls the method of a certain name for all items that are currently installed, similar to methodCall
      *
      * @param {string} method
      * @param {Array<any>} [args=[]]
@@ -71,4 +155,3 @@ export declare class Manager<K, V> extends Map<K, V> {
      */
     asyncMethodCall(method: string, ...args: any): Promise<Manager<K, V>>;
 }
-export default Manager;
