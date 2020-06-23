cad79b5092014cfed227d167cb0a855be9d59f2b
Okiki Ojo <femikiki@gmail.com>
2020-06-22 23:22:00 -0400

    update

--- packages/wali/docs/js/app.js
+++ packages/wali/docs/js/app.js
@@ -1,6 +1,475 @@
-function n(n){var t;if("undefined"!=typeof Symbol){if(Symbol.asyncIterator&&null!=(t=n[Symbol.asyncIterator]))return t.call(n);if(Symbol.iterator&&null!=(t=n[Symbol.iterator]))return t.call(n)}throw new TypeError("Object is not async iterable")}function t(n,e,i){if(!n.s){if(i instanceof r){if(!i.s)return void(i.o=t.bind(null,n,e));1&e&&(e=i.s),i=i.v;}if(i&&i.then)return void i.then(t.bind(null,n,e),t.bind(null,n,2));n.s=e,n.v=i;var u=n.o;u&&u(n);}}var e=function(){function t(n){this.map=new Map(n);}var e,r=t.prototype;return r.getMap=function(){return this.map},r.get=function(n){return this.map.get(n)},r.keys=function(){return [].concat(this.map.keys())},r.values=function(){return [].concat(this.map.values())},r.set=function(n,t){return this.map.set(n,t),this},r.add=function(n){return this.set(this.size,n),this},r.last=function(n){void 0===n&&(n=1);var t=this.keys()[this.size-n];return this.get(t)},r.prev=function(){return this.last(2)},r.delete=function(n){return this.map.delete(n),this},r.clear=function(){return this.map.clear(),this},r.has=function(n){return this.map.has(n)},r.entries=function(){return this.map.entries()},r.forEach=function(n,t){return void 0===n&&(n=function(){}),this.map.forEach(n,t),this},r[Symbol.iterator]=function(){return this.entries()},r.methodCall=function(n){var t=arguments;return this.forEach(function(e){e[n].apply(e,[].slice.call(t,1));}),this},r.asyncMethodCall=function(t){try{var e,r,i,h,c,a=this,s=arguments,l=!0,v=!1,d=f(function(){return o(function(){r=n(a.map);var e=u(function(){return !!Promise.resolve(r.next()).then(function(n){return l=i.done,i=n,Promise.resolve(i.value).then(function(n){return h=n,!l})})},function(){return !!(l=!0)},function(){var n=h[1];return Promise.resolve(n[t].apply(n,[].slice.call(s,1))).then(function(){})});if(e&&e.then)return e.then(function(){})},function(n){v=!0,c=n;})},function(n,t){function i(r){if(e)return r;if(n)throw t;return t}var u=f(function(){var n=function(){if(!l&&null!=r.return)return Promise.resolve(r.return()).then(function(){})}();if(n&&n.then)return n.then(function(){})},function(n,t){if(v)throw c;if(n)throw t;return t});return u&&u.then?u.then(i):i(u)});return Promise.resolve(d&&d.then?d.then(function(n){return e?n:a}):e?d:a)}catch(n){return Promise.reject(n)}},(e=[{key:"size",get:function(){return this.map.size}}])&&function(n,t){for(var e=0;e<t.length;e++){var r=t[e];r.enumerable=r.enumerable||!1,r.configurable=!0,"value"in r&&(r.writable=!0),Object.defineProperty(n,r.key,r);}}(t.prototype,e),t}(),r=function(){function n(){}return n.prototype.then=function(e,r){var i=new n,u=this.s;if(u){var o=1&u?e:r;if(o){try{t(i,1,o(this.v));}catch(n){t(i,2,n);}return i}return this}return this.o=function(n){try{var u=n.v;1&n.s?t(i,1,e?e(u):u):r?t(i,1,r(u)):t(i,2,u);}catch(n){t(i,2,n);}},i},n}();function i(n){return n instanceof r&&1&n.s}function u(n,e,u){for(var o;;){var f=n();if(i(f)&&(f=f.v),!f)return h;if(f.then){o=0;break}var h=u();if(h&&h.then){if(!i(h)){o=1;break}h=h.s;}if(e){var c=e();if(c&&c.then&&!i(c)){o=2;break}}}var a=new r,s=t.bind(null,a,2);return (0===o?f.then(v):1===o?h.then(l):c.then(d)).then(void 0,s),a;function l(r){h=r;do{if(e&&(c=e())&&c.then&&!i(c))return void c.then(d).then(void 0,s);if(!(f=n())||i(f)&&!f.v)return void t(a,1,h);if(f.then)return void f.then(v).then(void 0,s);i(h=u())&&(h=h.v);}while(!h||!h.then);h.then(l).then(void 0,s);}function v(n){n?(h=u())&&h.then?h.then(l).then(void 0,s):l(h):t(a,1,h);}function d(){(f=n())?f.then?f.then(v).then(void 0,s):v(f):t(a,1,h);}}function o(n,t){try{var e=n();}catch(n){return t(n)}return e&&e.then?e.then(void 0,t):e}function f(n,t){try{var e=n();}catch(n){return t(!0,n)}return e&&e.then?e.then(t.bind(null,!1),t.bind(null,!0)):t(!1,e)}
+/**
+ * Manages complex lists of named data, eg. A page can be stored in a list by of other pages with the url being how the page is stored in the list. Managers use Maps to store data.
+ *
+ * @export
+ * @class Manager
+ * @template K
+ * @template V
+ */
+class Manager {
+    /**
+     * Creates an instance of Manager.
+     *
+     * @param {Array<[K, V]>} [value]
+     * @memberof Manager
+     */
+    constructor(value) {
+        this.map = new Map(value);
+    }
+    /**
+     * Returns the Manager class's list
+     *
+     * @returns Map<K, V>
+     * @memberof Manager
+     */
+    getMap() {
+        return this.map;
+    }
+    /**
+     * Get a value stored in the Manager
+     *
+     * @public
+     * @param  {K} key - The key to find in the Manager's list
+     * @returns V
+     */
+    get(key) {
+        return this.map.get(key);
+    }
+    /**
+     * Returns the keys of all items stored in the Manager as an Array
+     *
+     * @returns Array<K>
+     * @memberof Manager
+     */
+    keys() {
+        return [...this.map.keys()];
+    }
+    /**
+     * Returns the values of all items stored in the Manager as an Array
+     *
+     * @returns Array<V>
+     * @memberof Manager
+     */
+    values() {
+        return [...this.map.values()];
+    }
+    /**
+     * Set a value stored in the Manager
+     *
+     * @public
+     * @param  {K} key - The key where the value will be stored
+     * @param  {V} value - The value to store
+     * @returns Manager<K, V>
+     */
+    set(key, value) {
+        this.map.set(key, value);
+        return this;
+    }
+    /**
+     * Adds a value to Manager, and uses the current size of the Manager as it's key, it works best when all the key in the Manager are numbers
+     *
+     * @public
+     * @param  {V} value
+     * @returns Manager<K, V>
+     */
+    add(value) {
+        // @ts-ignore
+        this.set(this.size, value);
+        return this;
+    }
+    /**
+     * Returns the total number of items stored in the Manager
+     *
+     * @public
+     * @returns Number
+     */
+    get size() {
+        return this.map.size;
+    }
+    /**
+     * Returns the last item in the Manager who's index is a certain distance from the last item in the Manager
+     *
+     * @param {number} [distance=1]
+     * @returns V | undefined
+     * @memberof Manager
+     */
+    last(distance = 1) {
+        let key = this.keys()[this.size - distance];
+        return this.get(key);
+    }
+    /**
+     * Returns the second last item in the Manager
+     *
+     * @public
+     * @returns V | undefined
+     */
+    prev() {
+        return this.last(2);
+    }
+    /**
+     * Removes a value stored in the Manager, via the key
+     *
+     * @public
+     * @param  {K} key - The key for the key value pair to be removed
+     * @returns Manager<K, V>
+     */
+    delete(key) {
+        this.map.delete(key);
+        return this;
+    }
+    /**
+     * Clear the Manager of all its contents
+     *
+     * @public
+     * @returns Manager<K, V>
+     */
+    clear() {
+        this.map.clear();
+        return this;
+    }
+    /**
+     * Checks if the Manager contains a certain key
+     *
+     * @public
+     * @param {K} key
+     * @returns boolean
+     */
+    has(key) {
+        return this.map.has(key);
+    }
+    /**
+     * Returns a new Iterator object that contains an array of [key, value] for each element in the Map object in insertion order.
+     *
+     * @public
+     * @returns IterableIterator<[K, V]>
+     */
+    entries() {
+        return this.map.entries();
+    }
+    /**
+     * Iterates through the Managers contents, calling a callback function every iteration
+     *
+     * @param {*} [callback=(...args: any): void => { }]
+     * @param {object} context
+     * @returns Manager<K, V>
+     * @memberof Manager
+     */
+    forEach(callback = (...args) => { }, context) {
+        this.map.forEach(callback, context);
+        return this;
+    }
+    /**
+     * Allows iteration via the for..of, learn more: [https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Iterators_and_Generators]
+     *
+     * @returns
+     * @memberof Manager
+     */
+    [Symbol.iterator]() {
+        return this.entries();
+    }
+    /**
+     * Calls the method of a certain name for all items that are currently installed
+     *
+     * @param {string} method
+     * @param {Array<any>} [args=[]]
+     * @returns Manager<K, V>
+     * @memberof Manager
+     */
+    methodCall(method, ...args) {
+        this.forEach((item) => {
+            item[method](...args);
+        });
+        return this;
+    }
+    /**
+     * Asynchronously calls the method of a certain name for all items that are currently installed, similar to methodCall
+     *
+     * @param {string} method
+     * @param {Array<any>} [args=[]]
+     * @returns Promise<Manager<K, V>>
+     * @memberof Manager
+     */
+    async asyncMethodCall(method, ...args) {
+        for await (let [, item] of this.map) {
+            await item[method](...args);
+        }
+        return this;
+    }
+}
 
-function e$1(t,e){t.prototype=Object.create(e.prototype),t.prototype.constructor=t,t.__proto__=e;}var n$1=function(){function t(t){var e=t.callback,n=t.scope,r=t.name;this.listener={callback:void 0===e?function(){}:e,scope:void 0===n?null:n,name:void 0===r?"event":r};}var e=t.prototype;return e.getCallback=function(){return this.listener.callback},e.getScope=function(){return this.listener.scope},e.getEventName=function(){return this.listener.name},e.toJSON=function(){return this.listener},t}(),r$1=function(t){function n(e){var n;return void 0===e&&(e="event"),(n=t.call(this)||this).name=e,n}return e$1(n,t),n}(e),i$1=function(t){function i(){return t.call(this)||this}e$1(i,t);var o=i.prototype;return o.getEvent=function(t){var e=this.get(t);return e instanceof r$1?e:(this.set(t,new r$1(t)),this.get(t))},o.newListener=function(t,e,r){var i=this.getEvent(t);return i.add(new n$1({name:t,callback:e,scope:r})),i},o.on=function(t,e,n){var r,i,o,c=this;return void 0===t||("string"==typeof t&&(t=t.split(/\s/g)),Object.keys(t).forEach(function(s){"object"!=typeof t||Array.isArray(t)?(r=t[s],i=e,o=n):(r=s,i=t[s],o=e),c.newListener(r,i,o);},this)),this},o.removeListener=function(t,e,r){var i=this.getEvent(t);if(e){for(var o,c=0,s=i.size,a=new n$1({name:t,callback:e,scope:r});c<s&&(o=i.get(c),console.log(o),o.getCallback()!==a.getCallback()||o.getScope()!==a.getScope());c++);i.delete(c);}return i},o.off=function(t,e,n){var r,i,o,c=this;return void 0===t||("string"==typeof t&&(t=t.split(/\s/g)),Object.keys(t).forEach(function(s){"object"!=typeof t||Array.isArray(t)?(r=t[s],i=e,o=n):(r=s,i=t[s],o=e),i?c.removeListener(r,i,o):c.delete(r);},this)),this},o.once=function(t,e,n){var r=this;return void 0===t||("string"==typeof t&&(t=t.split(/\s/g)),this.on(t,function i(){r.off(t,i,n),e.apply(n,[].slice.call(arguments));},n)),this},o.emit=function(t){var e=this,n=[].slice.call(arguments,1);return void 0===t||("string"==typeof t&&(t=t.split(/\s/g)),t.forEach(function(t){var r=e.getEvent(t),i=new CustomEvent(t,{detail:n});window.dispatchEvent(i),r.forEach(function(t){var e=t.toJSON();e.callback.apply(e.scope,n);});},this)),this},i}(e);
+/**
+ * Represents a new event listener consisting of properties like: callback, scope, name
+ *
+ * @export
+ * @class Listener
+ */
+class Listener {
+    /**
+     * Creates an instance of Listener.
+     *
+     * @param {IListener} { callback = () => { }, scope = null, name = "event" }
+     * @memberof Listener
+     */
+    constructor({ callback = () => { }, scope = null, name = "event", }) {
+        this.listener = { callback, scope, name };
+    }
+    /**
+     * Returns the callback Function of the Listener
+     *
+     * @returns ListenerCallback
+     * @memberof Listener
+     */
+    getCallback() {
+        return this.listener.callback;
+    }
+    /**
+     * Returns the scope as an Object, from the Listener
+     *
+     * @returns object
+     * @memberof Listener
+     */
+    getScope() {
+        return this.listener.scope;
+    }
+    /**
+     * Returns the event as a String, from the Listener
+     *
+     * @returns string
+     * @memberof Listener
+     */
+    getEventName() {
+        return this.listener.name;
+    }
+    /**
+     * Returns the listener as an Object
+     *
+     * @returns IListener
+     * @memberof Listener
+     */
+    toJSON() {
+        return this.listener;
+    }
+}
+/**
+ * Represents a new event listener consisting of properties like: callback, scope, name
+ *
+ * @export
+ * @class Event
+ * @extends {Manager<number, Listener>}
+ */
+class Event extends Manager {
+    /**
+     * Creates an instance of Event.
+     *
+     * @param {string} [name="event"]
+     * @memberof Event
+     */
+    constructor(name = "event") {
+        super();
+        this.name = name;
+    }
+}
+/**
+ * An event emitter
+ *
+ * @export
+ * @class EventEmitter
+ * @extends {Manager<string, Event>}
+ */
+class EventEmitter extends Manager {
+    /**
+     * Creates an instance of EventEmitter.
+     *
+     * @memberof EventEmitter
+     */
+    constructor() {
+        super();
+    }
+    /**
+     * Gets events, if event doesn't exist create a new Event
+     *
+     * @param {string} name
+     * @returns Event
+     * @memberof EventEmitter
+     */
+    getEvent(name) {
+        let event = this.get(name);
+        if (!(event instanceof Event)) {
+            this.set(name, new Event(name));
+            return this.get(name);
+        }
+        return event;
+    }
+    /**
+     * Creates a new listener and adds it to the event
+     *
+     * @param {string} name
+     * @param {ListenerCallback} callback
+     * @param {object} scope
+     * @returns Event
+     * @memberof EventEmitter
+     */
+    newListener(name, callback, scope) {
+        let event = this.getEvent(name);
+        event.add(new Listener({ name, callback, scope }));
+        return event;
+    }
+    /**
+     * Adds a listener for a given event
+     *
+     * @param {EventInput} events
+     * @param {ListenerCallback} callback
+     * @param {object} scope
+     * @returns
+     * @memberof EventEmitter
+     */
+    on(events, callback, scope) {
+        // If there is no event break
+        if (typeof events == "undefined")
+            return this;
+        // Create a new event every space
+        if (typeof events == "string")
+            events = events.split(/\s/g);
+        let _name;
+        let _callback;
+        let _scope;
+        // Loop through the list of events
+        Object.keys(events).forEach(key => {
+            // Select the name of the event from the list
+            // Remember events can be {String | Object | Array<string>}
+            // Check If events is an Object (JSON like Object, and not an Array)
+            if (typeof events == "object" && !Array.isArray(events)) {
+                _name = key;
+                _callback = events[key];
+                _scope = callback;
+            }
+            else {
+                _name = events[key];
+                _callback = callback;
+                _scope = scope;
+            }
+            this.newListener(_name, _callback, _scope);
+        }, this);
+        return this;
+    }
+    /**
+     * Removes a listener from an event
+     *
+     * @param {string} name
+     * @param {ListenerCallback} [callback]
+     * @param {object} [scope]
+     * @returns Event
+     * @memberof EventEmitter
+     */
+    removeListener(name, callback, scope) {
+        let event = this.getEvent(name);
+        if (callback) {
+            let i = 0, len = event.size, value;
+            let listener = new Listener({ name, callback, scope });
+            for (; i < len; i++) {
+                value = event.get(i);
+                console.log(value);
+                if (value.getCallback() === listener.getCallback() &&
+                    value.getScope() === listener.getScope())
+                    break;
+            }
+            event.delete(i);
+        }
+        return event;
+    }
+    /**
+     * Removes a listener from a given event, or it just completely removes an event
+     *
+     * @param {EventInput} events
+     * @param {ListenerCallback} [callback]
+     * @param {object} [scope]
+     * @returns EventEmitter
+     * @memberof EventEmitter
+     */
+    off(events, callback, scope) {
+        // If there is no event break
+        if (typeof events == "undefined")
+            return this;
+        // Create a new event every space
+        if (typeof events == "string")
+            events = events.split(/\s/g);
+        let _name;
+        let _callback;
+        let _scope;
+        // Loop through the list of events
+        Object.keys(events).forEach((key) => {
+            // Select the name of the event from the list
+            // Remember events can be {String | Object | Array<any>}
+            // Check If events is an Object (JSON like Object, and not an Array)
+            if (typeof events == "object" && !Array.isArray(events)) {
+                _name = key;
+                _callback = events[key];
+                _scope = callback;
+            }
+            else {
+                _name = events[key];
+                _callback = callback;
+                _scope = scope;
+            }
+            if (_callback) {
+                this.removeListener(_name, _callback, _scope);
+            }
+            else
+                this.delete(_name);
+        }, this);
+        return this;
+    }
+    /**
+     * Adds a one time event listener for an event
+     *
+     * @param {EventInput} events
+     * @param {ListenerCallback} callback
+     * @param {object} scope
+     * @returns EventEmitter
+     * @memberof EventEmitter
+     */
+    once(events, callback, scope) {
+        // If there is no event break
+        if (typeof events == "undefined")
+            return this;
+        // Create a new event every space
+        if (typeof events == "string")
+            events = events.split(/\s/g);
+        let onceFn = (...args) => {
+            this.off(events, onceFn, scope);
+            callback.apply(scope, args);
+        };
+        this.on(events, onceFn, scope);
+        return this;
+    }
+    /**
+     * Call all listeners within an event
+     *
+     * @param {(string | Array<any>)} events
+     * @param {...any} args
+     * @returns EventEmitter
+     * @memberof EventEmitter
+     */
+    emit(events, ...args) {
+        // If there is no event break
+        if (typeof events == "undefined")
+            return this;
+        // Create a new event every space
+        if (typeof events == "string")
+            events = events.split(/\s/g);
+        // Loop through the list of events
+        events.forEach((event) => {
+            let listeners = this.getEvent(event);
+            const customEvent = new CustomEvent(event, { detail: args });
+            window.dispatchEvent(customEvent);
+            listeners.forEach((listener) => {
+                let { callback, scope } = listener.toJSON();
+                callback.apply(scope, args);
+            });
+        }, this);
+        return this;
+    }
+}
 
 const getElements = (selector) => {
     return typeof selector === "string" ? Array.from(document.querySelectorAll(selector)) : [selector];
@@ -143,7 +612,7 @@ class Animate {
          * @type {EventEmitter}
          * @memberof Animate
          */
-        this.emitter = new i$1();
+        this.emitter = new EventEmitter();
         let { options: animation, ...rest } = options;
         this.options = Object.assign({}, DefaultAnimationOptions, animation, rest);
         let { loop, delay, speed, easing, endDelay, duration, direction, fillMode, onfinish, target, keyframes, autoplay, ...properties } = this.options;
