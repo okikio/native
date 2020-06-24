943bb043e0ec08cdec39a0735bb9c39327368592
Okiki Ojo <femikiki@gmail.com>
2020-06-24 18:14:12 -0400

    updated files

--- packages/manager/lib/api.d.ts
+++ packages/manager/lib/api.d.ts
@@ -132,7 +132,7 @@ export declare class Manager<K, V> {
     /**
      * Allows iteration via the for..of, learn more: [https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Iterators_and_Generators]
      *
-     * @returns
+     * @returns IterableIterator<[K, V]>
      * @memberof Manager
      */
     [Symbol.iterator](): IterableIterator<[K, V]>;
