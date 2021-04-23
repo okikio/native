eadddfc0321bf0a42e427e10be88e91002116240
Okiki <femikiki@gmail.com>
2021-04-23 08:38:57 +0000

    chore: save spot for Timeline class

--- packages/animate/src/api.ts
+++ packages/animate/src/api.ts
@@ -357,9 +357,9 @@ export class Animate {
     /**
      * Calls a method that affects all animations including the mainAnimation; the method only allows the animation parameter
     */
-    public all(method: (animation: Animation) => void) {
-        method(this.mainAnimation);
-        this.animations.forEach(animation => method(animation));
+    public all(method: (animation?: Animation, target?: HTMLElement) => void) {
+        method(this.mainAnimation, this.mainElement);
+        this.animations.forEach(method);
         return this;
     }
 
@@ -579,5 +579,117 @@ export class Animate {
 export const animate = (options: AnimationOptions = {}): Animate => {
     return new Animate(options);
 };
+
+export class Timeline extends Animate {
+    /**
+     * A Manager of Animations
+     */
+    public animations: Manager<HTMLElement, Animation> = new Manager();
+    constructor(options: AnimationOptions = {}) {
+        super();
+        try {
+            let { options: animation, ...rest } = options;
+            let oldOptions = animation instanceof Animate ? animation.getOptions() : (Array.isArray(animation) ? animation?.[0]?.getOptions() : animation);
+            this.options = Object.assign({}, DefaultAnimationOptions, oldOptions, rest);
+            this.loop = this.loop.bind(this);
+
+            let {
+                loop,
+                delay,
+                speed,
+                easing,
+                endDelay,
+                duration,
+                direction,
+                fillMode,
+                onfinish,
+                target,
+                keyframes,
+                autoplay,
+                extend,
+                ...properties
+            } = this.options;
+
+            this.mainElement = document.createElement("div");
+            this.targets = getTargets(target);
+            this.properties = properties;
+
+            let delays = [];
+            let len = this.targets.length;
+            let animationKeyframe: Keyframe[] | PropertyIndexedKeyframes;
+            for (let i = 0; i < len; i++) {
+                let target = this.targets[i] as HTMLElement;
+                let animationOptions: AnimationOptions = {
+                    easing: typeof easing == "string" ? getEase(easing) : easing,
+                    iterations: loop === true ? Infinity : (loop as number),
+                    direction,
+                    endDelay,
+                    duration,
+                    delay,
+                    fill: fillMode,
+                    ...extend
+                };
+
+                // Accept keyframes as a keyframes Object, or a method,
+                // if there are no animations in the keyframes array,
+                // uses css properties from the options object
+                let arrKeyframes = computeValue((keyframes as Keyframe[]), [i, len, target], this);
+                animationKeyframe = arrKeyframes.length ? arrKeyframes :
+                    (this.properties as PropertyIndexedKeyframes);
+
+                // Allows the use of functions as the values, for both the keyframes and the animation object
+                // It adds the capability of advanced stagger animation, similar to the anime js stagger functions
+                animationOptions = mapObject(animationOptions, [i, len, target], this);
+                if (!(arrKeyframes.length > 0))
+                    animationKeyframe = mapObject(animationKeyframe, [i, len, target], this);
+
+                // Set the Animate classes duration to be the Animation with the largest totalDuration
+                let tempDurations = animationOptions.delay +
+                    (animationOptions.duration * animationOptions.iterations) +
+                    animationOptions.endDelay;
+                if (this.totalDuration < tempDurations) this.totalDuration = tempDurations;
+
+                // Add animation to the Animations Set
+                let animation = target.animate(animationKeyframe, animationOptions as KeyframeAnimationOptions);
+
+                // Support for on finish
+                animation.onfinish = () => {
+                    typeof onfinish == "function" && onfinish.call(this, target, i, len, animation);
+                    this.emit("finish", target, i, len, animation);
+                };
+
+                // The calculated options for each individual option
+                this.computedOptions.set(animation, animationOptions);
+                this.animations.set(target, animation);
+                delays.push(animationOptions.delay);
+            }
+
+            this.mainAnimation = this.mainElement.animate([
+                { opacity: "0" },
+                { opacity: "1" }
+            ], {
+                // Why waste performance on an animation no one can see?
+                duration: this.totalDuration,
+                easing: "linear"
+            });
+
+            this.minDelay = Math.min(...delays);
+            this.setSpeed(speed);
+            if (autoplay) this.play();
+            else this.pause();
+
+            this.promise = this.newPromise();
+            this.mainAnimation.onfinish = () => {
+                this.emit("complete", this);
+                this.stopLoop();
+            };
+        } catch (err) {
+            this.emit("error", err);
+        }
+    }
+    public add(options: AnimationOptions) {
+        return this;
+    }
+}
 
 export default animate;
