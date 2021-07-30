7b14728159a05856b4a9747d392aaa1903f28758
Okiki <femikiki@gmail.com>
2021-07-30 20:15:45 +0000

    use rollup for build process

--- build.js
+++ build.js
@@ -1,93 +1,137 @@
 import chalk from "chalk";
-import { build } from "esbuild";
-import { file } from "gzip-size";
-import prettyBytes from "pretty-bytes";
+import { rollup } from "rollup";
+import { terser } from "rollup-plugin-terser";
 
-let arrPromises, promise, newTime, oldTime;
-export default async (source = "src/api.ts", globalName, mode) => {
-    const fileSize = async (inputFile = "lib/api.mjs") => `${prettyBytes(await file(inputFile))}`;
-    const outputs = [
-        {
-            outfile: "lib/api.mjs",
-            format: "esm",
-            target: ["es2020"],
-        },
-        {
-            outfile: "lib/api.js",
-            format: "iife",
-            target: ["es2020"],
-        },
-        {
-            outfile: "lib/api.cjs",
-            platform: "node",
-            target: ["es2020"],
-            format: "cjs",
-        },
-    ];
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from "@rollup/plugin-commonjs";
 
-    let buildData = {
-        entryPoints: [source],
-        color: true,
-        bundle: true,
-        minify: true,
-        sourcemap: false,
-        globalName,
-        tsconfig: "./tsconfig.json",
-        logLevel: "info",
-    };
+import bundleSize from "rollup-plugin-bundle-size";
+import esbuild from "rollup-plugin-esbuild";
 
-    // Build code
-    arrPromises = await Promise.all(
-        outputs.map((output) => {
-            return build({
-                ...buildData,
-                ...output,
-                incremental: true,
-            });
-        })
-    );
+import { readFileSync } from "fs";
+import { globby } from 'globby';
 
-    newTime = Date.now();
+import path from "path";
+import { fileURLToPath } from "url";
 
-    if (mode == "watch") {
-        const { watch } = await import("chokidar");
-        const watcher = watch(["src/**/*"]);
-        console.log(chalk`Watching {gray ${globalName}/${source}} and surrounding files for changes... \n`);
+import minimist from "minimist";
 
-        watcher.on("change", () => {
-            oldTime = Date.now();
-            (async () => {
-                let timeStart = Date.now();
-                for (let i = 0; i < arrPromises.length; i++) {
-                    oldTime = Date.now();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-                    let { outfile } = outputs[i];
-                    promise = arrPromises[i];
-                    arrPromises[i] = await promise.rebuild();
+/**
+ * @param {string}[scope] - packages to only build (if you don't
+ *    want to build everything)
+ * @param {string}[ignore] - packages to not build
+ *
+ * @returns {string[]} - sorted list of Package objects that
+ *    represent packages to be built.
+ */
+export const getSortedPackages = async (scope = [], ignore = []) => {
+    scope = [].concat(scope);
+    ignore = [].concat(ignore);
+    return (await globby(["packages"], {
+        expandDirectories: scope.length ? scope.map(v => `${v.replace("@okikio/", "")}/package.json`) : ["package.json"],
+        ignore: ignore.length ? ignore.map(v => `packages/${v.replace("@okikio/", "")}/package.json`) : [],
+        deep: 2
+    })).map(file => ({
+        file: file,
+        location: file.replace("package.json", ""),
+        json: JSON.parse(readFileSync(file, "utf-8"))
+    }))
+};
 
-                    newTime = Date.now();
-                    console.log(chalk`Built {red ${outfile}} in {yellow ${newTime - oldTime}ms}, file size {green ${await fileSize(outfile)}} Gzipped`);
-                }
+export const rollupConfig = async ({ scope, ignore } = {}) => {
+    /**
+     * @type {import('rollup').RollupOptions}
+     */
+    const config = [];
+    const packages = await getSortedPackages(scope, ignore);
 
-                let timeEnd = Date.now();
-                console.log(chalk`Total build time is: {red ${timeEnd - timeStart}ms}\n`);
-            })();
+    packages.forEach((pkg) => {
+        /* Absolute path to package directory */
+        const basePath = path.relative(__dirname, pkg.location);
+
+        let pkgObj = pkg.json;
+
+        /* Absolute path to input file */
+        const input = path.join(basePath, pkgObj.main);
+
+        /* "main" & "module" field from package.json file. */
+        const { main, module, umd: name, legacy } = pkgObj.publishConfig;
+
+        /* Push build config for this package. */
+        config.push({
+            input,
+            output: [
+                {
+                    file: path.join(basePath, module),
+                    exports: "named",
+                    format: "es"
+                },
+                {
+                    file: path.join(basePath, main),
+                    exports: "named",
+                    format: "cjs",
+                },
+                {
+                    file: path.join(basePath, legacy),
+                    exports: "named",
+                    name,
+                    format: "umd",
+                },
+            ],
         });
-    } else {
-        let timeStart = Date.now();
-        for (let i = 0; i < arrPromises.length; i++) {
-            oldTime = Date.now();
+    });
 
-            let { outfile } = outputs[i];
-            promise = arrPromises[i];
-            await promise.rebuild();
-            promise.rebuild.dispose();
+    return config;
+};
 
-            newTime = Date.now();
-            console.log(chalk`Built {red ${outfile}} in {yellow ${newTime - oldTime}ms}, file size {green ${await fileSize(outfile)}} Gzipped`);
+// Support --scope and --ignore globs if passed in via commandline
+const args = minimist(process.argv.slice(2));
+const configs = await rollupConfig(args);
+
+export async function build() {
+    for (let { input, output } of configs) {
+        let title = chalk`{gray Bundled in}`;
+        console.time(title);
+        console.log(`\n` + chalk`Building {red ${input}}`)
+
+        // Create a bundle
+        const bundle = await rollup({
+            input,
+            plugins: [
+                esbuild({
+                    target: "es2021", // default, or 'es20XX', 'esnext'
+                    logLevel: "info",
+                
+                    color: true,
+                    bundle: true,
+                    minify: true,
+                
+                    sourcemap: false,
+                    platform: "browser",
+                    tsconfig: "./tsconfig.json",
+                }),
+                nodeResolve(),
+                commonjs(),
+                bundleSize(),
+                terser({
+                    ecma: 2021,
+                    compress: true, mangle: true
+                })
+            ]
+        });
+
+        // or write the bundle to disk
+        for (let outputOption of output) {
+            await bundle.write(outputOption);
         }
 
-        let timeEnd = Date.now();
-        console.log(chalk`Total build time is: {red ${timeEnd - timeStart}ms}`);
+        // closes the bundle
+        await bundle.close();
+        console.timeEnd(title);
     }
-};
+}
+
+build();
