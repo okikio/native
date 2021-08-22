f588f80815789120323dd4fa436f4d02d308ad53
Okiki <okikio.dev@gmail.com>
2021-08-22 04:12:50 +0000

    update esbuild

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ export default {
   // dist: './dist',       // When running `astro build`, path to final static output
   // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
   buildOptions: {
-    site: 'https://okikio.github.io/native',  // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-    // sitemap: true,      // Generate sitemap (set to "false" to disable)
+    site: 'https://native.okikio.dev/',  // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+    sitemap: true,      // Generate sitemap (set to "false" to disable)
   },
   markdownOptions: plugins,
   devOptions: {
