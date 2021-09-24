4d27b79a4140396531354c3dd57c41ff2ba43666
Okiki <okikio.dev@gmail.com>
2021-09-24 03:04:25 -0400

    fix: fix redirect bug

--- src/markdown-plugins.js
+++ src/markdown-plugins.js
@@ -45,11 +45,11 @@ const __dirname = path.resolve(path.dirname(""));
 // console.log(fs.existsSync(path.join(__dirname, `/docs/animate`)));
 function redirectDocsURL(url) {
     if (/^\/docs/.test(url.path)) {
-        if (fs.existsSync(path.join(__dirname, url.path))) {
-            return url.path.replace(/^\/docs\//, "/").replace(/\.md$/, "");
-        } else {
-            return `https://github.com/okikio/native/tree/beta` + url.path;
-        }
+       // if (fs.existsSync(path.join(__dirname, url.path))) {
+           return url.path.replace(/^\/docs\//, "/").replace(/\.md$/, "");
+        // } else {
+         //   return `https://github.com/okikio/native/tree/beta` + url.path;
+        // }
     } else if (/\.md$/.test(url.path)) {
         return url.path.replace(/\.md$/, "");
     } else if (/^\/packages/.test(url.path)) {
